f4fec4ef9bc51e9b60cb61bd838d888a37f93fa8
James Boyer <jamesboyer92@user.noreply.github.com>
2020-01-13 10:08:33 -0500

    created tests for input, and layed out ground work for input component

---
+++ src/Input.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+import Input from './Input'
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders input without error', () => {
+
+        });
+        test('renders the input box', () => {
+
+        });
+        test('renders submit button', () => {
+
+        });
+    });
+
+    describe('word has been guessed', () => {
+        test('renders without error', () => {
+
+        });
+        test('does not render input box', () => {
+
+        });
+        test('does not render submitButton', () => {
+
+        });
+    });
+});
+
+describe('updates state', () => {
+
+});
+
